6200b6fcf5b20e40b5d3c439fd26df39585ab43a
Clement IACOBUCCI <clement.iacobucci@gmail.com>
2020-10-11 21:24:34 +0200

    feat(filtres): filtres terminés

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,21 +1,18 @@
 import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import {ApiService} from '../services/api.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
 
-  products: Observable<Product[]>;
+  @Input() products: Observable<Product[]>;
 
-  constructor(private apiService: ApiService) { }
+  constructor() { }
 
-  ngOnInit(){
-    this.products = this.apiService.getProducts();
-  }
 }
